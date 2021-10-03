cca6571583866243e6e1d66d4f4d6644e1497f7e
qwertypnk <qwertypnk@protonmail.com>
2021-10-03 20:28:55 +0300

    fix: smallfix

--- mobile.entry.js
+++ mobile.entry.js
@@ -1,1 +1,6 @@
-/Users/qwertypnk/sotaoi/sotaoieco/app-mobile/index.js
+require('@sotaoi/config').Config.init(require('@app/mobile/env.json'));
+require('react-native-gesture-handler');
+const { App } = require('@app/mobile/lib/main');
+const { AppRegistry } = require('react-native');
+
+AppRegistry.registerComponent('qwertypnksmobileboilerplate', () => App);
