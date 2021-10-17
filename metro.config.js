9e3004a6328b2dee10eb48c13e2cbc1f908757e4
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 16:46:21 +0300

    refactor: update

--- metro.config.js
+++ metro.config.js
@@ -3,19 +3,19 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 const { getDefaultConfig } = require('metro-config');
 
 const extraNodeModules = {
-  react: path.dirname(require.resolve('@sotaoi/client/node_modules/react')),
-  'react-dom': path.dirname(require.resolve('@sotaoi/client/node_modules/react-dom')),
-  'react-native': path.resolve('./node_modules/react-native'),
-  redux: path.resolve('./node_modules/redux'),
-  'react-redux': path.resolve('./node_modules/react-redux'),
+  // react: path.dirname(require.resolve('@sotaoi/client/node_modules/react')),
+  // 'react-dom': path.dirname(require.resolve('@sotaoi/client/node_modules/react-dom')),
+  // 'react-native': path.resolve('./node_modules/react-native'),
+  // redux: path.resolve('./node_modules/redux'),
+  // 'react-redux': path.resolve('./node_modules/react-redux'),
 };
 
 const blacklistRegexes = [/build[\/\\].*/, /deployment[\/\\].*/, /scripts[\/\\].*/, /var[\/\\].*/];
 const watchFolders = [
-  path.resolve('./../app-client'),
-  path.resolve('./../app-omni'),
-  path.resolve('./../packages/sotaoi-client'),
-  path.resolve('./../packages/sotaoi-omni'),
+  // path.resolve('./../app-client'),
+  // path.resolve('./../app-omni'),
+  // path.resolve('./../packages/sotaoi-client'),
+  // path.resolve('./../packages/sotaoi-omni'),
 ];
 
 module.exports = (async () => {
